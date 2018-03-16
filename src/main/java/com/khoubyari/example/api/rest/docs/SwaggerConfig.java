122865c0e08abeed886c20c87555fcdca4811533
amitmindstix <amit.mujawar@mindstix.com>
2018-03-16 14:33:55 +0530

    Update SwaggerConfig.java

--- src/main/java/com/khoubyari/example/api/rest/docs/SwaggerConfig.java
+++ src/main/java/com/khoubyari/example/api/rest/docs/SwaggerConfig.java
@@ -39,7 +39,7 @@ public class SwaggerConfig {
                 .termsOfServiceUrl("github")
                 .license("Mindstix Labs")
                 .licenseUrl("")
-                .version("1.3")
+                .version("1.3a")
  //               .contact(new Contact("siamak"))
                 .build();
     }
